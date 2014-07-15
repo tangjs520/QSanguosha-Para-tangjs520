6569bda3cdc617b2d32fc3e3131890fdd29d2e22
xiaoilyrx@gmail.com <1qaz2wsx>
2014-07-15 23:34:38 +0800

    add open animation

--- lang/zh_CN/Common.lua
+++ lang/zh_CN/Common.lua
@@ -219,6 +219,8 @@ CommonTranslationTable = {
 	["CAPITAL(8)"] = "八",
 	["CAPITAL(9)"] = "九",
 	["CAPITAL(10)"] = "十",
+	
+	["$gamestart"] = "image=image/animate/gamestart.png",
 }
 
 return CommonTranslationTable
